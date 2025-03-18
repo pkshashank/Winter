429f26b358bb9b1a160a13ef30cfb514861fc233
Shashank Pathak <pathakshashank97@gmail.com>
2025-03-18 17:46:04 +0000

    show all parses

--- Main.hs
+++ Main.hs
@@ -6,6 +6,7 @@ import System.Process (callCommand)
 import qualified Data.Text.Lazy as T
 import qualified Data.Text.Lazy.IO as TIO
 import LogicalTheory
+import System.Posix.Internals (puts)
 
 
 
@@ -14,19 +15,23 @@ main = do
   content <- readFile "test.txt"
 
   pgf <- readPGF "Math.pgf"
-  let lang =  head $ languages pgf
+  let lang =  head $ languages pgf -- The language is called MathEng
 
 
-  let pgfTree = head $ parse pgf lang (fromJust $ readType "Sentence") content
+  let pgfTrees = parse pgf lang (fromJust $ readType "Sentence") content
+  let pgfTree = head pgfTrees
+  let noOfTrees = length pgfTrees
   producePNG pgf pgfTree
+  putStrLn "\nThese are all the parse trees. We work with the first one."
+  showAllTrees pgfTrees
 
   putStr "\n1. PARSE TREE (ast.png): \n"
   putStrLn (showExpr [mkCId "Sentence"] pgfTree)
   let haskellTree = Math.fg pgfTree :: Math.GSentence
-  putStrLn "\n2. PERFORMING SEMANTIC ANALYSIS...\n"
+  putStrLn "\n2. PERFORMING SEMANTIC ANALYSIS..."
 
   formula <- translate haskellTree
-  putStr "\n3. TRANSLATED FORMULA: \n "
+  putStr "\n3. TRANSLATED FORMULA:\n"
   case formula of
     Left err -> print err
     Right f -> print f
@@ -39,3 +44,9 @@ producePNG pgf pgfTree = do
   TIO.writeFile "ast.dot" $ T.pack graph
   callCommand "dot -Tpng ast.dot -o ast.png"
   return ()
+  
+showAllTrees :: [PGF.Tree] -> IO ()
+showAllTrees pgfTrees = do
+  case pgfTrees of
+    [] -> return ()
+    (pgfTree:pgfTrees) -> putStrLn (showExpr [mkCId "Sentence"] pgfTree) >> showAllTrees pgfTrees
